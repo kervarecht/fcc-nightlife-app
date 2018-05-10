adc6f26c84329ff6d443c892688702cf288c1c23
Thibault <kervarecht@gmail.com>
2018-05-10 13:24:52 -0400

    Port error on production build

--- apiserver.js
+++ apiserver.js
@@ -181,7 +181,7 @@ app.get('/logout', (req, res) => {
     res.redirect('/');
 })
 //=====PORT AND RUN====//
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.listen(port, () => {
     console.log("Listening on port " + port);
